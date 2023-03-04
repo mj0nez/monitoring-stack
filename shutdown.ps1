2078c82e7af292fffe29af9a1b3e3df638d0b238
Marcel Johannesmann <mj0nez@fn.de>
2023-03-04 19:22:58 +0100

    integrated docker monitoring and separated compose files

--- shutdown.ps1
+++ shutdown.ps1
@@ -1,7 +1,7 @@
 # Script to stop the logging and monitoring layer consecutively
 
 # the monitoring layer: grafana, timescale ...
-docker compose -f docker-compose.yml stop
+docker compose -f compose-observe.yml stop
 
 # the logging layer: loki, prometheus
-docker compose -f docker-compose-loki.yml stop
+docker compose -f compose-infra.yml stop
