1294f84849d6de81ba4c178ec91e78985299083d
Marcel Johannesmann <mj0nez@fn.de>
2023-03-08 12:08:49 +0100

    separated metrics from infra layer

--- down.ps1
+++ down.ps1
@@ -1,7 +1,5 @@
 # Script to startup the logging and monitoring layer consecutively
 
-# the monitoring layer: grafana, timescale ...
 docker compose -f compose-observe.yml down
-
-# the logging layer: loki, prometheus
+docker compose -f compose-metrics.yml down
 docker compose -f compose-infra.yml down
--- up.ps1
+++ up.ps1
@@ -1,7 +1,5 @@
 # Script to startup the logging and monitoring layer consecutively
 
-# the logging layer: loki, prometheus
 docker compose -f compose-infra.yml up -d
-
-# the monitoring layer: grafana, timescale ...
+docker compose -f compose-metrics.yml up -d
 docker compose -f compose-observe.yml up -d
